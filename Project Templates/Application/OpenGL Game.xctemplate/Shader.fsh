70bb1d179179bc5cff339bed0f5c60eaaf4c0e75
Matt Di Pasquale <liveloveprosper@gmail.com>
2013-03-18 12:14:13 -0400

    Add Xcode 4.6 (4H127) Project Templates.

---
+++ Project Templates/Application/OpenGL Game.xctemplate/Shader.fsh
@@ -0,0 +1,14 @@
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+varying lowp vec4 colorVarying;
+
+void main()
+{
+    gl_FragColor = colorVarying;
+}
---
+++ Project Templates/Application/OpenGL Game.xctemplate/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ___VARIABLE_classPrefix:identifier___ViewController : GLKViewController
+
+@end
