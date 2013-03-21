f75c99b88d925b3fbc7e398a38fed93fdcf0a689
Matt Di Pasquale <liveloveprosper@gmail.com>
2013-03-20 20:50:58 -0400

    Test Template: Mv .h file into .m file.

--- Project Templates/Base/Objective-C Unit Test Bundle.xctemplate/___PACKAGENAMEASIDENTIFIER___.m
+++ Project Templates/Base/Objective-C Unit Test Bundle.xctemplate/___PACKAGENAMEASIDENTIFIER___.m
@@ -1,32 +1,25 @@
-//
-//  ___FILENAME___
-//  ___PACKAGENAME___
-//
-//  Created by ___FULLUSERNAME___ on ___DATE___.
-//___COPYRIGHT___
-//
+#import <SenTestingKit/SenTestingKit.h>
 
-#import "___FILEBASENAME___.h"
+@interface ___FILEBASENAMEASIDENTIFIER___ : SenTestCase
+@end
 
 @implementation ___FILEBASENAMEASIDENTIFIER___
 
 - (void)setUp
 {
     [super setUp];
-    
     // Set-up code here.
 }
 
 - (void)tearDown
 {
     // Tear-down code here.
-    
     [super tearDown];
 }
 
 - (void)testExample
 {
-    STFail(@"Unit tests are not implemented yet in ___PACKAGENAME___");
+    STFail(@"Unit tests are not implemented yet in ___PACKAGENAME___.");
 }
 
 @end
